5fa1dd18c5483c8b0377508774f087c1e4a160e2
LiviaEster <liviaester16@gmail.com>
2019-03-20 20:34:48 -0300

    versao 0.1

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package padroesdeprojeto;
+
+
+/**
+ *
+ * @author livia
+ */
+public class Calculadora {
+      Adicao adição; 
+      Subtracao subtracao;
+      Multiplicacao multiplicacao;
+      Divisao divisao;
+
+}
