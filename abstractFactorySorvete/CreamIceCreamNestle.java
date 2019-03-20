12ef054349319c752a2c51bad46258f96b7e46cd
LiviaEster <liviaester16@gmail.com>
2019-03-20 20:33:16 -0300

    versao 0.1

---
+++ abstractFactorySorvete/CreamIceCreamNestle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractFactory;
+
+/**
+ *
+ * @author livia
+ */
+public class CreamIceCreamNestle implements CreamIceCream{
+
+    @Override
+    public void exibirInfoCreamIceCream() {
+        System.out.println("Sabor:Creme Marca:Nestle ");
+    }
+    
+}
